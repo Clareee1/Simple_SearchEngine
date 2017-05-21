ef0b66d52706dbd18bbb78ddb31af3e8df4f878c
HenryQuan <natequan@hotmail.com>
2017-05-21 20:21:46 +1000

    Update testUnit
    Test for getNumFromString

--- testUnit.c
+++ testUnit.c
@@ -45,6 +45,10 @@ void testParser() {
     int t2_2 = getNumFromString("url888");
     if (t2_2 != 888) fatalError("getNumFromString part 2");
     else testPassed("getNumFromString part 2");
+    // Testing getNumFromString part3
+    int t2_3 = getNumFromString("url/url231.txt");
+    if (t2_3 != 231) fatalError("getNumFromString part 3");
+    else testPassed("getNumFromString part 3");
 
     // Testing getNameOfUrlFromFile
     int *t3 = getNameOfUrlFromFile("url/collection.txt", t1);
@@ -67,18 +71,18 @@ void testParser() {
         char name[64];
         sprintf(name, "url/url%d.txt", t3[t4]);
         printf("url%d: \n", t3[t4]);
-        int urlCount = getUrlTxtFromFile(name, urlArray);
+        // Check for url count
+        int urlCount = getUrlFromFile(name, urlArray);
         printf("%d url%s in %s\n", urlCount, urlCount == 0 ? "" : "s", name);
         int i;
-        for (i = 0; i < t1; i++) {
+        for (i = 0; i < urlCount; i++) {
             if (urlArray[i] == 0) continue;
-            printf("url%d.txt ", urlArray[i]);
+            printf("url%d\n", urlArray[i]);
             urlArray[i] = 0;
         }
         printf("\n\n");
     }
-    testPassed("getUrlTxtFromFile");
-
+    testPassed("getUrlFromFile / getTxtFromFile");
 }
 
 // Show fatal error and exit
