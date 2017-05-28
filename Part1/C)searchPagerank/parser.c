d74f3f921c2615ddd1a8f445ac9e8839eb8c65b4
v-raina <alina.aldhytha@gmail.com>
2017-05-28 23:37:32 +1000

    Part 1c draft 1 (without comments)

---
+++ Part1/C)searchPagerank/parser.c
@@ -0,0 +1,246 @@
+//
+//  parser.c
+//  Created by Henry Quan on 15/05/17.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "parser.h"
+
+#define MAX_BUFFER 64
+
+// Convert a string to lower (Henry)
+void strlower(char* str) {
+    if (str == NULL) return;
+    int len = strlen(str);
+    int i;
+    for (i = 0; i < len; i++) {
+        // Convert to lowercase if it is uppercase
+        if (isupper(str[i])) str[i] = tolower(str[i]);
+    }
+}
+
+// Get all links from a given webpage (Henry)
+int getUrlFromFile(char *name, int *urlArray) {
+    if (name == NULL || urlArray == NULL) return -1;
+
+    FILE *fp = fopen(name, "r");
+    if (fp != NULL) {
+        // Scan all text
+        char buffer[MAX_BUFFER];
+        int sectionCount = 0;
+        int urlCount = 0;
+        while (fscanf(fp, "\n %s ", buffer) == 1) {
+            // End of section-1 and the start of section-2
+            if (strcmp(buffer, "Section-1") == 0 || strcmp(buffer, "#end") == 0) sectionCount++;
+            else if (strstr(buffer, "url")) {
+                // YEAH! A url
+                urlArray[urlCount] = getNumFromString(buffer);
+                // printf("url%d.txt\n", urlArray[urlCount]);
+                urlCount++;
+            } else if (sectionCount == 2) {
+                // End of urls
+                break;
+            }
+        }
+        // printf("\n");
+        return urlCount;
+    }
+    return -1;
+}
+
+// Get all links from a given webpage (Henry)
+/*void getTxtFromFile(char *name, Tree bst) {
+    if (name == NULL) return;
+    FILE *fp = fopen(name, "r");
+    if (fp != NULL) {
+        // Scan all text
+        char buffer[64];
+        int sectionCount = 0;
+        while (fscanf(fp, "\n %s ", buffer) == 1) {
+            // End of section-1 and the start of section-2
+            if (strcmp(buffer, "Section-2") == 0 || strcmp(buffer, "#end") == 0) sectionCount++;
+            else if (sectionCount == 2) { // This is a keyword
+                // If it has a full stop at the end, remove it
+                if (buffer[strlen(buffer) - 1] == '.') buffer[strlen(buffer) - 1] = '\0';
+                // Convert it to lower cased
+                strlower(buffer);
+                // Add it to Binary Tree (Alina)
+                int url = getNumFromString(name);
+                TreeFindAndInsert(bst, buffer, url);
+            } else if (sectionCount == 3) break; // Done
+        }
+    }
+}*/
+
+// Get name of links from collection.txt (Henry)
+int *getNameOfUrlFromFile(char *name, int num) {
+    if (name == NULL || num <= 0) return NULL;
+
+    int *nameArray = calloc(num, sizeof(int));
+    if (nameArray == NULL) exit(1);
+
+    FILE *fp = fopen(name, "r");
+    // Be able to open file
+    if (fp != NULL) {
+        int i, currNum = 0;
+        for (i = 0; i < num; i++) {
+            // Read all number into array
+            fscanf(fp, "\nurl%d ", &currNum);
+            nameArray[i] = currNum;
+        }
+    }
+    fclose(fp);
+    return nameArray;
+}
+
+// Get number of links from collection.txt (Henry)
+int getNumOfUrlFromFile(char *name) {
+    if (name == NULL) return -1;
+
+    int numOfUrl = 0;
+    FILE *fp = fopen(name, "r");
+    // Be able to open file
+    if (fp != NULL) {
+        char buffer[MAX_BUFFER];
+        while (fscanf(fp, "\n %s ", buffer) == 1) {
+            if (strstr(buffer, "url")) numOfUrl++;
+        }
+    }
+    fclose(fp);
+    return numOfUrl;
+}
+
+// Get the number of links for a keyword from invertedIndex.txt (Part 1B) (Alina)
+// Added for convenience purposes (Alina)
+int getNumOfUrlForKeywordFromFile(char *name, char *keyword) {
+    if (name == NULL) return -1;
+
+    FILE *fp = fopen(name, "r");
+    int count = 0;
+    if (fp != NULL) {
+        char buffer[512];
+        // Read line by line until keyword is reached
+        while (fgets(buffer, 511, fp) != NULL) {
+            if (strstr(buffer, keyword)) {
+                char *url = strtok(buffer, " ");
+                while ((url = strtok(NULL, " "))) {
+                    if (strstr(url, "url")) {
+                        count++;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    fclose(fp);
+    return count;
+}
+
+// Get the name of links for a keyword from invertedIndex.txt (Part 2) (Henry)
+// Changed the return value to none (Alina)
+void getNameOfUrlForKeywordFromFile(char *name, char *keyword, int *array) {
+    if (name == NULL) return;
+
+    FILE *fp = fopen(name, "r");
+    int count = 0;
+    if (fp != NULL) {
+        char buffer[512];
+        // Read line by line until keyword is reached
+        while (fgets(buffer, 511, fp) != NULL) {
+            if (strstr(buffer, keyword)) {
+                char *url = strtok(buffer, " ");
+                while ((url = strtok(NULL, " "))) {
+                    if (strstr(url, "url")) {
+                        array[count] = getNumFromString(url);
+                        count++;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    fclose(fp);
+}
+
+// Get number of keywords from invertedIndex.txt (Part 2) (Henry)
+int getNumOfKeywordFromFile(char *name) {
+    if (name == NULL) return -1;
+
+    FILE *fp = fopen(name, "r");
+    int count = 0;
+    if (fp != NULL) {
+        char c;
+        // Read how many lines we have
+        while ((c = getc(fp)) != EOF) {
+            if (c == '\n') count++;
+        }
+    }
+    fclose(fp);
+    return count;
+}
+
+// Count how many times a keyword appeared in one url (Part 2) (Henry)
+int getKeywordCountFromUrl(char *name, char *keyword) {
+    if (name == NULL || keyword == NULL) return -1;
+
+    FILE *fp = fopen(name, "r");
+    int count = 0;
+    if (fp != NULL) {
+        char buffer[64];
+        while (fscanf(fp, "\n %s ", buffer) == 1) {
+            strlower(buffer);
+            // We found that word, YEAH!
+            if (strcmp(buffer, keyword) == 0) count++;
+        }
+    }
+    fclose(fp);
+    return count;
+}
+
+// Check if there is such keywords from invertedIndex.txt (Part 2) (Henry)
+int hasKeyword(char *name, char *keyword) {
+    if (name == NULL) return -1;
+
+    FILE *fp = fopen(name, "r");
+    int count = 0;
+    if (fp != NULL) {
+        char buffer[MAX_BUFFER];
+        while (fscanf(fp, "\n %s ", buffer) == 1) {
+            strlower(buffer);
+            // We found that word, YEAH!
+            if (strcmp(buffer, keyword) == 0) {
+                count = 1;
+                break;
+            }
+        }
+    }
+    fclose(fp);
+    return count;
+}
+
+// Convert url23.txt to 23 (Henry)
+int getNumFromString(char *name) {
+    if (name == NULL) return -1;
+
+    int num = 0;
+    // Get  number from string
+    if (sscanf(name, "%*[^0123456789]%d%*[^0123456789]", &num) == 1) {
+        return num;
+    }
+    return -1;
+}
+
+// Convert 123 to url123 (Alina)
+char * getStringFromNum (int num) {
+    int size = snprintf (NULL, 0, "url%d.txt", num);
+    char * name = malloc ((size + 1) * sizeof(char));
+    if (name == NULL) {
+        printf("Out of Memory.\n");
+        exit (EXIT_FAILURE);
+    }
+    snprintf(name, size + 1, "url%d", num);
+    return name;
+}
---
+++ Part1/C)searchPagerank/searchPagerank.c
@@ -0,0 +1,84 @@
+//searchPagerank.c
+// Written by Aldhytha Karina Sari (Alina), 26 May 2017
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "parser.h"
+
+#define TRUE 0
+#define FALSE 1
+#define MAX_URL_TO_PRINT 10
+#define MAX_URL 100
+#define MAX_BUFFER 64
+
+int arrayConjunction (int *, int *, int *, int, int);
+
+int main (int argc, char * argv[]) {
+	if (argc == 3) {
+		char * keyword1 = argv[1]; 
+		char * keyword2 = argv[2];
+
+		if ((hasKeyword("invertedIndex.txt", keyword1)) == 0 || (hasKeyword("invertedIndex.txt", keyword2)) == 0) {
+			printf("Enter a valid keyword\n");
+			exit(EXIT_SUCCESS);
+		}
+
+		int arraySize1 = getNumOfUrlForKeywordFromFile("invertedIndex.txt", keyword1);
+		int urlArray1[arraySize1];
+		getNameOfUrlForKeywordFromFile("invertedIndex.txt", keyword1, urlArray1);
+
+		int arraySize2 = getNumOfUrlForKeywordFromFile("invertedIndex.txt", keyword2);
+		int urlArray2[arraySize2];
+		getNameOfUrlForKeywordFromFile("invertedIndex.txt", keyword2, urlArray2);
+
+		int combinedSize;
+
+		if ((arraySize1 < arraySize2) == TRUE) {
+			combinedSize = arraySize1;
+		} else {
+			combinedSize = arraySize2;
+		}
+
+		int combined [combinedSize];
+
+		combinedSize = arrayConjunction (combined, urlArray1, urlArray2, arraySize1, arraySize2);
+
+		FILE *fp = fopen ("pagerankList.txt", "r");
+		if (fp == NULL) {
+			printf("Out of Memory\n");
+			exit (EXIT_FAILURE);
+		}
+
+		char buffer [MAX_BUFFER];
+	    while (fscanf(fp, "\n %s ", buffer) == 1) {
+            strlower(buffer);
+            int i;
+            for (i = 0; i < combinedSize; i++) {
+            	char * tmp = getStringFromNum (combined[i]);
+	    	    if (strstr(buffer, tmp) != NULL) {
+	    	    	printf("%s\n", tmp);
+	    	    }
+	    	    free(tmp);
+		    }
+    	}
+		fclose (fp);
+	} else {
+		printf("Usage: ./searchPagerank [keyword1] [keyword2]\n");
+	}
+	return EXIT_SUCCESS;
+}
+
+int arrayConjunction (int * combined, int *array1, int *array2, int size1, int size2) {
+	int i, j;
+	int combinedSize = 0;
+	for (i = 0; i < size1; i++) {
+		for (j = 0; j < size2; j++) {
+			if (array1[i] == array2[j]) {
+				combined[combinedSize] = array1[i];
+				combinedSize++;
+			}
+		}
+	}
+	return combinedSize;
+}
