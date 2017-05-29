63174f7bcbd6556af6a6af4d59ac552b5bf43fd9
v-raina <alina.aldhytha@gmail.com>
2017-05-29 16:48:13 +1000

    Can have more than 2 keywords as inputs

--- Part1/C)searchPagerank/searchPagerank.c
+++ Part1/C)searchPagerank/searchPagerank.c
@@ -8,78 +8,85 @@
 
 #define TRUE 0
 #define FALSE 1
+#define MAX_URL 100
 #define MAX_URL_TO_PRINT 10
 #define MAX_BUFFER 64
 
-int arrayConjunction (int *, int *, int *, int, int);
+int * createArray (int size);
+void arrayUnion (int *, int *, int, int);
+void printArray (int * array);
 
 int main (int argc, char * argv[]) {
-	if (argc == 3) {
-		char * keyword1 = argv[1]; 
-		char * keyword2 = argv[2];
 
-		if ((hasKeyword("invertedIndex.txt", keyword1)) == 0 || (hasKeyword("invertedIndex.txt", keyword2)) == 0) {
-			printf("Enter a valid keyword\n");
-			exit(EXIT_SUCCESS);
+	if (argc > 2) {
+		int * combined = createArray (MAX_URL);
+		int counter;
+		for (counter = 1; counter < argc; counter++) {
+			if (hasKeyword ("invertedIndex.txt", argv[counter]) == 0) {
+				exit(EXIT_SUCCESS);
+			} 
+
+			int arraySize = getNumOfUrlForKeywordFromFile("invertedIndex.txt", argv[counter]);
+			int * urlArray = createArray (arraySize);
+			getNameOfUrlForKeywordFromFile("invertedIndex.txt", argv[counter], urlArray);
+
+			arrayUnion (combined, urlArray, MAX_URL, arraySize);
+
+			free (urlArray);
 		}
-
-		int arraySize1 = getNumOfUrlForKeywordFromFile("invertedIndex.txt", keyword1);
-		int urlArray1[arraySize1];
-		getNameOfUrlForKeywordFromFile("invertedIndex.txt", keyword1, urlArray1);
-
-		int arraySize2 = getNumOfUrlForKeywordFromFile("invertedIndex.txt", keyword2);
-		int urlArray2[arraySize2];
-		getNameOfUrlForKeywordFromFile("invertedIndex.txt", keyword2, urlArray2);
-
-		int combinedSize;
-
-		if ((arraySize1 < arraySize2) == TRUE) {
-			combinedSize = arraySize1;
-		} else {
-			combinedSize = arraySize2;
-		}
-
-		int combined [combinedSize];
-
-		combinedSize = arrayConjunction (combined, urlArray1, urlArray2, arraySize1, arraySize2);
-
-		FILE *fp = fopen ("pagerankList.txt", "r");
-		if (fp == NULL) {
-			printf("Out of Memory\n");
-			exit (EXIT_FAILURE);
-		}
-
-		char buffer [MAX_BUFFER];
-	    while (fscanf(fp, "\n %s ", buffer) == 1) {
-            strlower(buffer);
-            int i;
-            int count = 0;
-            for (i = 0; i < combinedSize; i++) {
-            	char * tmp = getStringFromNum (combined[i]);
-	    	    if (strstr(buffer, tmp) != NULL && count < 10) {
-	    	    	printf("%s\n", tmp);
-	    	    	count++;
-	    	    }
-	    	    free(tmp);
-		    }
-    	}
-		fclose (fp);
+		printArray (combined);
+   		free (combined);
 	} else {
-		printf("Usage: ./searchPagerank [keyword1] [keyword2]\n");
+		exit (EXIT_SUCCESS);
 	}
 	return EXIT_SUCCESS;
 }
 
-int arrayConjunction (int * combined, int *array1, int *array2, int size1, int size2) {
+int * createArray (int size) {
+	int * array = malloc (size * sizeof (int));
+	if (array == NULL) {
+		exit (EXIT_FAILURE);
+	}
+	int i;
+	for (i = 0; i < size; i++) {
+		array[i] = -1;
+	}
+	return array;
+}
+
+void arrayUnion (int * result, int * input, int resultSize, int inputSize) {
 	int i, j;
-	int combinedSize = 0;
-	for (i = 0; i < size1; i++) {
-		for (j = 0; j < size2; j++) {
-			if (array1[i] == array2[j]) {
-				combined[combinedSize] = array1[i];
-				combinedSize++;
+	for (i = 0; i < inputSize; i++) {
+		for (j = 0; result[j] != -1 && j < resultSize; j++) {
+			if (result[j] == input[i]) {
+				break;
 			}
 		}
+		if (result[j] == -1) {
+			result[j] = input[i];
+		}
 	}
-	return combinedSize;
+}
+
+void printArray (int * array) {
+	FILE *fp = fopen ("pagerankList.txt", "r");
+	if (fp == NULL) {
+		printf("Out of Memory\n");
+		exit (EXIT_FAILURE);
+	}
+	char buffer [MAX_BUFFER];
+    while (fscanf(fp, "\n %s ", buffer) == 1) {
+     	strlower(buffer);
+        int i;
+        int count = 0;
+		for (i = 0; array[i] != -1; i++) {
+     	   	char * tmp = getStringFromNum (array[i]);
+    	    if (strstr(buffer, tmp) != NULL && count < MAX_URL_TO_PRINT) {
+    	   		printf("%s\n", tmp);
+    	   		count++;
+	        }
+	        free(tmp);
+	    }
+    }
+	fclose (fp);
 }
