9686502aec6d8f4a696df60321707cd3430fef4b
HenryQuan <natequan@hotmail.com>
2017-06-02 16:20:43 +1000

    Update Part2
    Please check for free()

--- Submission/searchTfIdf.c
+++ Submission/searchTfIdf.c
@@ -33,6 +33,12 @@ void getTopTen(double *resultArray, int *urlArray, int size);
 void fatalError(char *message);
 // Remove duplicates
 void keywordsWithoutDuplicates(char *argv[], int argc);
+// duplicate array
+void arrayCopy(int *, int *, int);
+// find intersection of arrays
+int arrayConjunction(int *, int *, int *, int, int);
+// New array
+int *createArray(int size);
 
 int main(int argc, char *argv[]) {
     // There has to be at least one words
@@ -45,12 +51,46 @@ int main(int argc, char *argv[]) {
     double *resultArray = newDoubleArray(totalUrlCount);
     if (resultArray == NULL) fatalError("Out of memory");
     // Create another temp array with same size to hold temp url
-    int *tempArray = newIntArray(totalUrlCount);
-    if (tempArray == NULL) fatalError("Out of memory");
+    // int *tempArray = newIntArray(totalUrlCount);
+    // if (tempArray == NULL) fatalError("Out of memory");
 
     // Store the index for all urls
     int *urlArray = getNameOfUrlFromFile("collection.txt", totalUrlCount);
     if (urlArray == NULL) fatalError("Out of memory");
+
+    // Make new array for the urls in the intersection of urls
+    int *combined = createArray(argc - 1);
+    int combinedSize = 0;
+    int counter;
+    for (counter = 1; counter < argc; counter++) {
+        if (argv[counter] == NULL) continue;
+        if (hasKeyword("invertedIndex.txt", argv[counter]) == 0) {
+            free(combined);
+            exit(EXIT_SUCCESS);
+        }
+        //get the array of url that contains the keyword
+        int arraySize = getNumOfUrlForKeywordFromFile("invertedIndex.txt", argv[counter]);
+        if (arraySize == 0) {
+            free(combined);
+            exit(EXIT_SUCCESS);
+        }
+        int * urlArray = createArray(arraySize);
+        getNameOfUrlForKeywordFromFile("invertedIndex.txt", argv[counter], urlArray);
+
+        //adjust the combined array
+        if (counter == 1) {
+            arrayCopy(combined, urlArray, arraySize);
+            combinedSize = arraySize;
+        } else {
+            int * tmp = createArray(combinedSize);
+            arrayCopy(tmp, combined, combinedSize);
+            combinedSize = arrayConjunction(combined, tmp, urlArray, combinedSize, arraySize);
+            free(tmp);
+        }
+
+        //free the array of urls of one keyword
+        free (urlArray);
+    }
 
     int i, j;
     for (i = 1; i < argc; i++) {
@@ -58,26 +98,23 @@ int main(int argc, char *argv[]) {
             if (!hasKeyword("invertedIndex.txt", argv[i])) continue;
             // Repeat for each keyword
             int urlWithKeyword = getNumOfUrlForKeywordFromFile("invertedIndex.txt", argv[i]);
-            getNameOfUrlForKeywordFromFile("invertedIndex.txt", argv[i], tempArray);
+            // getNameOfUrlForKeywordFromFile("invertedIndex.txt", argv[i], tempArray);
             double idf = calIdfValue(urlWithKeyword, totalUrlCount);
             // printf("c: %d, idf: %f\n", urlWithKeyword, idf);
-            for (j = 0; j < urlWithKeyword; j++) {
+            for (j = 0; j < combinedSize; j++) {
                 // Repeat for each url
                 char buffer[64]; // Store url full name
-                sprintf(buffer, "url%d.txt", tempArray[j]);
+                sprintf(buffer, "url%d.txt", combined[j]);
                 // Get term frequency
                 int tf = getKeywordCountFromUrl(buffer, argv[i]);
                 // Get index
-                int index = getIndexFromArray(urlArray, tempArray[j], totalUrlCount);
+                int index = getIndexFromArray(urlArray, combined[j], totalUrlCount);
                 // printf("tf: %d, index: %d, idtdf: %f\n", tf, index, calTfidfValue(calTfValue(tf), idf));
                 // Add this value to resultArray if valid
                 if (index != -1) resultArray[index] += calTfidfValue(calTfValue(tf), idf);
             }
         }
     }
-
-    // Free tempArray
-    free(tempArray);
 
     // Sort resultArray
     getTopTen(resultArray, urlArray, totalUrlCount);
@@ -87,10 +124,50 @@ int main(int argc, char *argv[]) {
         printf("url%d, %0.6f\n", urlArray[i], resultArray[i]);
     }
 
+    free(combined);
     free(urlArray);
     free(resultArray);
 
     return 0;
+}
+
+// mallocked new array
+int *createArray (int size) {
+	//mallocked a new array
+	int * array = malloc (size * sizeof (int));
+	if (array == NULL) {
+		exit (EXIT_FAILURE);
+	}
+	//set all values to -1
+	int i;
+	for (i = 0; i < size; i++) {
+		array[i] = -1;
+	}
+	return array;
+}
+
+// duplicate array
+void arrayCopy (int * result, int * input, int sizeInput) {
+	int i;
+	for (i = 0; i < sizeInput; i++) {
+		result[i] = input[i];
+	}
+}
+
+// find intersection of arrays
+int arrayConjunction (int * result, int * input1, int * input2, int arraySize1, int arraySize2) {
+	int i, j;
+	int length = 0;
+	for (i = 0; i < arraySize1; i++) {
+		for (j = 0; j < arraySize2; j++) {
+			if (input1[i] == input2[j]) {
+				//copy urls that are in both input arrays to result array
+				result[length] = input1[i];
+				length++;
+			}
+		}
+	}
+	return length;
 }
 
 // Remove duplicates
